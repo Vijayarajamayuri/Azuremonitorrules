5a340edf4fff606963665df411ef3f754def3855
mayuri vijayaraja akula <46686521+Vijayarajamayuri@users.noreply.github.com>
2025-05-29 10:44:28 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,22 @@
 variable "metricAlerts" {
   type = map(object({
     alertName                  = string
-    # alertScopes                = list(string)
+    alertScopes                = list(string)
+    alertDescription           = string
+    alertEnabled               = bool
+    alertAutoMitigate          = bool
+    alertFrequency             = string
+    alertTargetResourceType    = string
+    alertTargetResourceLoc     = string
+    criteria                   = map(string)
+  }))
+  default = {}
+}
+
+variable "metricAlerts" {
+  type = map(object({
+    alertName                  = string
+    alertScopes                = list(string)
     alertDescription           = string
     alertEnabled               = bool
     alertAutoMitigate          = bool
@@ -21,10 +36,25 @@ variable "metricAlerts" {
         values   = list(string)
       })), [])
     })), [])
+    dynamic_criteria = optional(list(object({
+      metric_namespace         = string
+      metric_name              = string
+      aggregation              = string
+      operator                 = string
+      alert_sensitivity        = optional(string, "Medium")
+      evaluation_total_count   = optional(number, 4)
+      evaluation_failure_count = optional(number, 4)
+      ignore_data_before       = optional(string)
+      skip_metric_validation   = optional(bool, false)
+      dimension = optional(list(object({
+        name     = string
+        operator = optional(string, "Include")
+        values   = list(string)
+      })), [])
+    })), [])
   }))
   default = {}
 }
-
 
 variable "monitor_scoped_mysql" {
   description = "List of MySQL servers to be monitored"
@@ -35,3 +65,6 @@ variable "monitor_scoped_mysql" {
   }))
   default = []
 }
+
+
+
